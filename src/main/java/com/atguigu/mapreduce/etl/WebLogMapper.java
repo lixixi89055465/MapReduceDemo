1f9ff3645440cfe2e020693ad6ce1327874655ca
lixixi89055465 <nanji1850094299@outlook.com>
2022-11-09 15:51:04 +0800

    data

---
+++ src/main/java/com/atguigu/mapreduce/etl/WebLogMapper.java
@@ -0,0 +1,39 @@
+package com.atguigu.mapreduce.etl;
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+public class WebLogMapper extends Mapper<LongWritable, Text, Text, NullWritable> {
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        //1 获取一行
+        String line = value.toString();
+
+        //2 切割
+
+        // 3 etl
+        boolean result = parseLog(line, context);
+        if (!result) {
+            return;
+        }
+        // 4 写出
+        context.write(value, NullWritable.get());
+    }
+
+    private boolean parseLog(String line, Context context) {
+        // 切割
+        String[] fields = line.split(" ");
+        //2 判断一下日志的长度是否大于 11
+        if (fields.length > 11) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+}
